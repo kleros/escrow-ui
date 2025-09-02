5e7f46695b90571301abc17f24c0b3d1ceea381a
kyrers <kyrers.eth@gmail.com>
2025-09-02 18:29:04 +0100

    feat: add KlerosLiquid ABI and generate wagmi hooks

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@wagmi/cli";
 import { react } from "@wagmi/cli/plugins";
 import { MULTIPLE_ARBITRABLE_TRANSACTION_ABI } from "./src/config/contracts/abi/multipleArbitrableTransaction";
 import { MULTIPLE_ARBITRABLE_TOKEN_TRANSACTION_ABI } from "./src/config/contracts/abi/mutlipleArbitrableTokenTransaction";
+import { KLEROS_LIQUID_ABI } from "./src/config/contracts/abi/klerosLiquid";
 
 export default defineConfig({
   out: "src/config/contracts/generated.ts",
@@ -13,6 +14,10 @@ export default defineConfig({
     {
       name: "MultipleArbitrableTokenTransaction",
       abi: MULTIPLE_ARBITRABLE_TOKEN_TRANSACTION_ABI,
+    },
+    {
+      name: "KlerosLiquid",
+      abi: KLEROS_LIQUID_ABI,
     },
   ],
   plugins: [react()],
